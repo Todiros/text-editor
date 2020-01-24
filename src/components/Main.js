6170bdb5da5a66c1d1eb408e9a595af7f88fd3cd
Tony Borisov <tony.d.borisov@gmail.com>
2020-01-24 20:11:21 +0200

    style: code cleanup and basic npm setup

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main className="App">
+        <p>
+          Edit <code>src/Main.js</code> and save to reload.
+        </p>
+    </main>
+  );
+}
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/Main.sass';
-import App from './components/App';
+import Main from './components/Main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
